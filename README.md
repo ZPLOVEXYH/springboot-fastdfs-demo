b2bb26e1481fa2bca0b9a3375f1f1355d69d6e8c
ZPLOVEXYH <1031626336@qq.com>
2019-09-12 16:27:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# SpringBoot + FastDFS 简单 DEMO
+
+## 开发环境
+- FastDFS 5.12
+- CentOS 6.7
+
+## 目录
+1. [安装和部署](markdown-file/1.安装和部署.md)
+2. [FastDFS与SpringBoot集成](markdown-file/2.FastDFS与SpringBoot集成.md) 
---
+++ src/main/java/com/gitnavi/springboot/springbootsimpledemo/common/FastDFSConstants.java
@@ -0,0 +1,10 @@
+package com.gitnavi.springboot.springbootsimpledemo.common;
+
+public class FastDFSConstants {
+
+    public static final String HTTP_PRODOCOL = "http";
+    public static final String RES_HOST = "192.168.237.129";
+    public static final String RES_PORT = "8888";
+
+
+}
